6f415a9f6cbcf964377d532d14cbce58a7323974
saranca14 <61488445+saranca14@users.noreply.github.com>
2023-06-21 22:24:13 +0200

    Update install_kubeadm.sh
    folder

--- tools/k8s/kubeadm/install_kubeadm.sh
+++ tools/k8s/kubeadm/install_kubeadm.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+#!/bin/bash
+
+#Update the system
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+#Make the directory if not present
+dir="/etc/apt/keyrings"
+if [ -d "$dir" ];
+then
+echo "dir exists"
+else
+mkdir $dir
+fi
+
+#Download the Google Cloud public signing key
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+
+#Add the Kubernetes apt repository:
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
+sudo apt-get update
+sudo apt-get install -y kubelet=1.27.0-00 kubeadm=1.27.0-00 kubectl=1.27.0-00
+sudo apt-mark hold kubelet kubeadm kubectl
